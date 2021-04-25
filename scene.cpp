e0891dc8adfb421ce35509294cfe5df3520268b7
Tulenien <timofey.evsigneev@gmail.com>
2021-04-25 20:31:58 +0300

    Add class Scene with constructor, draw pixels on screen

---
+++ scene.cpp
@@ -0,0 +1,15 @@
+﻿#include "scene.h"
+
+Scene::Scene(QWidget *parent)
+{
+    QSize size = this->size();
+    qDebug() << size;
+    image = new QImage(size, QImage::Format_RGBA8888);
+    QColor line_color(0, 0, 0);
+    for(int i = 0; i < size.width(); i++)
+    {
+        for(int j = 0; j < size.height(); j++)
+        image->setPixel(i, j, line_color.rgba());
+    }
+    this->setPixmap(QPixmap::fromImage(*image));
+}
---
+++ scene.h
@@ -0,0 +1,32 @@
+﻿#ifndef SCENE_H
+#define SCENE_H
+
+#include <QLabel>
+#include <QDebug>
+// Add ZBuffer and
+// find the way to change it whenever object rotate
+
+
+class Scene : public QLabel
+{
+    Q_OBJECT
+public:
+    QImage *image;
+    Scene(QWidget *parent);
+    void setWidth(qreal width);
+    void setLength(qreal length);
+    void setHeight(qreal height);
+    qreal getWidth();
+    qreal getLength();
+    qreal getHeight();
+    void rotate(qreal angleX, qreal angleY);
+    void render();
+
+private:
+    qreal width, length, height;
+    QImage *buffer;
+    void transform(qreal distance);
+    void draw();
+};
+
+#endif // SCENE_H
