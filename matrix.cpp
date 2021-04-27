8e7747c7f99d9ff0a097f0a44278a8560c8797bb
Tulenien <timofey.evsigneev@gmail.com>
2021-04-27 04:22:03 +0300

    Add Matrix class realisation

---
+++ matrix.cpp
@@ -0,0 +1,74 @@
+﻿#include "matrix.h"
+
+Matrix::Matrix(int rows, int cols)
+{
+    rows_num = rows;
+    cols_num = cols;
+    container = new double* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        container[i] = new double[cols];
+    }
+}
+
+Matrix::~Matrix()
+{
+    for(int i = 0; i < rows_num; i++)
+    {
+        delete [] container[i];
+    }
+    delete [] container;
+    rows_num = 0;
+    cols_num = 0;
+}
+
+void Matrix::FillwithArray(double **array)
+{
+    for(int i = 0; i < rows_num; i++)
+    {
+        for(int j = 0; i < cols_num; j++)
+        {
+            container[i][j] = *array[i * cols_num + j];
+        }
+    }
+}
+
+void Matrix::Multiply(const Matrix &right, Matrix &result)
+{
+    // Standart multiplication algorithm
+    int rows = this->rows_num;
+    int cols = right.cols_num;
+    int nest = this->cols_num;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result.container[i][j] = 0;
+            for (int k = 0; k < nest; k++)
+            {
+                result.container[i][j] =
+                 this->container[i][k] * right.container[k][j];
+            }
+        }
+    }
+}
+
+void Matrix::setRows(int rows)
+{
+    rows_num = rows;
+}
+
+void Matrix::setCols(int cols)
+{
+    cols_num = cols;
+}
+
+int Matrix::getRows()
+{
+    return rows_num;
+}
+
+int Matrix::getCols()
+{
+    return cols_num;
+}
---
+++ matrix.h
@@ -0,0 +1,26 @@
+﻿#ifndef MATRIX_H
+#define MATRIX_H
+
+
+#define OK 0
+#define ERROR 1
+
+class Matrix
+{
+public:
+    Matrix(int, int);
+    ~Matrix();
+    void FillwithArray(double **array);
+    void Multiply(const Matrix &, Matrix &);
+    int getRows();
+    int getCols();
+    void setRows(int rows);
+    void setCols(int cols);
+
+private:
+    double **container;
+    int rows_num;
+    int cols_num;
+};
+
+#endif // MATRIX_H
